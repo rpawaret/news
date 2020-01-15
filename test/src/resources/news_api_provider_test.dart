ced436769e143ec0ab7f9f39ed328d91edc9bfc1
pawaret <pawaret.r@gmail.com>
2020-01-16 00:35:27 +0700

    226

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -2,3 +2,10 @@ import 'package:news/src/resources/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
+
+void main(List<String> args) {
+  test('FetchTopIds returns a list ids', () {
+    // setup of test case
+    // expectation
+  });
+}
