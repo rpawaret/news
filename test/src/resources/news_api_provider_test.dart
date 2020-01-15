e390c1f5500beb679ed08dffc86dac2f5f008c50
pawaret <pawaret.r@gmail.com>
2020-01-16 00:41:52 +0700

    227

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,3 +1,4 @@
+import 'package:http/testing.dart';
 import 'package:news/src/resources/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
@@ -6,6 +7,10 @@ import 'package:http/http.dart';
 void main(List<String> args) {
   test('FetchTopIds returns a list ids', () {
     // setup of test case
+    final newsApi = NewsApiProvider();
+    MockClient((request) {
+      
+    })
     // expectation
   });
 }
