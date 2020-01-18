6d10dafa731002e19b6da26685c08e745e993d9f
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-19 00:51:44 +0700

    262

---
+++ lib/src/blocs/stories_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_bloc.dart';
+
+class StoriesProvider extends InheritedWidget {
+  final StoriesBloc bloc;
+
+  StoriesProvider({Key key, Widget child})
+      : bloc = StoriesBloc(),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
+  }
+
+  static StoriesBloc of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<StoriesProvider>().bloc;
+  }
+}
