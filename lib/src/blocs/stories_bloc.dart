b53e56c639eebc4a3491eb3cc0f1d6d6d16c1b91
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-19 15:58:53 +0700

    274

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -5,8 +5,11 @@ import '../models/item_model.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _item = BehaviorSubject<int>();
 
   Stream<List<int>> get topIds => _topIds.stream;
+
+  Function(int) get fetchItem => _item.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -15,11 +18,14 @@ class StoriesBloc {
 
   _itemTransformer() {
     return ScanStreamTransformer(
-        (Map<int, Future<ItemModel>> cache, int id, _) {},
-        <int, Future<ItemModel>>{});
+        (Map<int, Future<ItemModel>> cache, int id, _) {
+      cache[id] = _repository.fetchItem(id);
+      return cache;
+    }, <int, Future<ItemModel>>{});
   }
 
   dispose() {
     _topIds.close();
+    _item.close();
   }
 }
