85cc8a77323095bd6998b0517b53393ff79808c8
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-19 14:56:21 +0700

    264

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,9 +1,16 @@
+import 'package:news/src/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class StoriesBloc {
+  final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
 
   Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
 
   dispose() {
     _topIds.close();
