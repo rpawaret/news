3e1a6b7ac18edeff8bb34bed8d50c922d9b3c1c9
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-19 14:51:17 +0700

    263

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,11 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  Stream<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
+}
