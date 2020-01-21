b4f5ee9e15c88e426860180313dff4ec1cd03c66
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-21 21:50:38 +0700

    278

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/models/item_model.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'dart:async';
 
 class NewsListTile extends StatelessWidget {
   final int itemId;
@@ -9,6 +11,13 @@ class NewsListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
-    return null;
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        } else {}
+      },
+    );
   }
 }
