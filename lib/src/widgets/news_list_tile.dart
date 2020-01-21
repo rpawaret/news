d2dd4b35c003418f1ccb69934723b4217f42ba4c
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-22 02:00:50 +0700

    290

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -36,12 +36,22 @@ class NewsListTile extends StatelessWidget {
   }
 
   Widget buildTile(ItemModel item) {
-    return ListTile(
-      title: Text(item.title),
-      subtitle: Text('${item.score} points'),
-      trailing: Column(
-        children: <Widget>[Icon(Icons.comment), Text('${item.descendants}')],
-      ),
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(item.title),
+          subtitle: Text('${item.score} points'),
+          trailing: Column(
+            children: <Widget>[
+              Icon(Icons.comment),
+              Text('${item.descendants}')
+            ],
+          ),
+        ),
+        Divider(
+          height: 8,
+        )
+      ],
     );
   }
 }
