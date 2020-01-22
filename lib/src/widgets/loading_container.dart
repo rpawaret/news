a9f62998e64040d3eb454b3854113a7adf5b4b86
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-23 01:17:30 +0700

    292

--- lib/src/widgets/loading_container.dart
+++ lib/src/widgets/loading_container.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class LoadingContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: buildContainer(),
+          subtitle: buildContainer(),
+        ),
+        Divider(
+          height: 8,
+        )
+      ],
+    );
+  }
+
+  Widget buildContainer() {
+    return Container(
+      color: Colors.grey[200],
+      height: 24,
+      width: 150,
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+    );
   }
 }
