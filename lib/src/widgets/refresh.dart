37faa2142a6047fc9efbeca19c94c2e939221197
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-23 21:44:52 +0700

    298

--- lib/src/widgets/refresh.dart
+++ lib/src/widgets/refresh.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_provider.dart';
 
 class Refresh extends StatelessWidget {
   final Widget child;
 
-  Refresh({this.child})
+  Refresh({this.child}) {}
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(child: child, onRefresh: () {
-      
-    },);
+    final bloc = StoriesProvider.of(context);
+    return RefreshIndicator(
+      child: child,
+      onRefresh: () async {
+        await bloc.clearCache();
+        await bloc.fetchTopIds();
+      },
+    );
   }
-
 }
