613709e8d1b97256848dc920aa294f27a78becae
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-25 18:27:45 +0700

    316

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -34,10 +34,21 @@ class NewsDetail extends StatelessWidget {
               return Text('Loading');
             }
 
-            return Text('${itemSnapshot.data.title}');
+            return buildTitle(itemSnapshot.data);
           },
         );
       },
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
