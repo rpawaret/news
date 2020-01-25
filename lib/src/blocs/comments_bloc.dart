c1c9712b49a68b10a875657183da2311ee6aa686
Pawaret Rakkamnoet <pawaret.r@gmail.com>
2020-01-25 16:35:56 +0700

    309

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,22 @@
-class CommentsBloc {}
+import 'dart:async';
+
+import 'package:rxdart/subjects.dart';
+
+import '../models/item_model.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  // Streams
+  Stream<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  // Sinks
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
